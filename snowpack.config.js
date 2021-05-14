ae463265995cd6da6a30faf733ff20b75c85ca15
davidpodhola <david@podhola.net>
2021-05-14 09:34:03 +0200

    add storybook

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,6 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
+    exclude: ['**/node_modules/**/*', '**/stories/**/*'],
     mount: {
         public: { url: '/', static: true },
         src: { url: '/dist' },
---
+++ src/stories/App.stories.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import App from "../App.svelte";
+</script>
+
+<Meta
+  title="Example/App"
+  component={App}
+  argTypes={{
+  }}
+/>
+
+<Template let:args>
+  <App {...args}  />
+</Template>
+
+<Story
+  name="Primary"
+  args={{
+
+  }}
+/>
